a9d8cfa93d4b8f04aa768ac5239aabb37f6b9674
Oleg Vilkov <oleg.vilkov.work@gmail.com>
2020-08-16 21:12:14 +0300

    Рефакторинг компонента "Сделать ход"

--- src/components/GameMove.tsx
+++ src/components/GameMove.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-import { BlockTitle, BlockHeader, Block, Button, List, ListItem, Stepper } from 'framework7-react';
+import { BlockHeader, Block, Button, List, ListItem, Stepper } from 'framework7-react';
 
+/**
+ * Компонент для угадывания чёт-нечет и ввод нового числа с кнопкой «Отправить»
+ */
 export default function GameMove () {
     return (
         <>
-        <BlockTitle>Угадайте четное или нечетное число загадал предыдущий участник</BlockTitle>
-        <BlockHeader>Icon in the beginning of the list item</BlockHeader>
+        <BlockHeader>Угадайте четное или нечетное число загадал предыдущий участник</BlockHeader>
         <List>
             <ListItem radio value="check_1" name="demo-radio" title="Чётное" defaultChecked />
             <ListItem radio value="check_2" name="demo-radio" title="Нечётное" />
         </List>
-        <BlockTitle>Загадайте своё число для следующего игрока</BlockTitle>
+        <BlockHeader>Загадайте своё число для следующего игрока</BlockHeader>
         <Block className="text-align-center">
           <Stepper fill onStepperChange={()=>{}}></Stepper>
         </Block>
