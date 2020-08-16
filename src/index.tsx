fa32d5bf995a386dbb8e25e46e2f01bb342d1ed2
Oleg Vilkov <oleg.vilkov.work@gmail.com>
2020-08-16 14:49:58 +0300

    Added Framework7 in Project

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// Import F7 Bundle
+import Framework7 from 'framework7/framework7-lite.esm.bundle.js';
+
+// Import F7-React Plugin
+import Framework7React from 'framework7-react';
+
+// Init F7-React Plugin
+Framework7.use(Framework7React);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +18,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+// Import pages components
+import HomePage from './pages/HomePage';
+// import AboutPage from 'about.jsx';
+// import LoginPage from 'login.jsx';
+
+export default [
+    {
+        path: '/',
+        component: HomePage,
+    },
+    // {
+    //   path: '/about/',
+    //   component: AboutPage,
+    // },
+    // {
+    //   path: '/login/',
+    //   component: LoginPage,
+    // },
+];
