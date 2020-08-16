c1e78a1ae9a853c2404a0638dc52ba91ca5e404e
Oleg Vilkov <oleg.vilkov.work@gmail.com>
2020-08-16 18:21:50 +0300

    Четное выбрано по умолчанию

--- src/components/GameMove.tsx
+++ src/components/GameMove.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import { BlockTitle, Block, Button, List, ListItem, Stepper } from 'framework7-react';
+import { BlockTitle, BlockHeader, Block, Button, List, ListItem, Stepper } from 'framework7-react';
 
 export default function GameMove () {
     return (
         <>
         <BlockTitle>Угадайте четное или нечетное число загадал предыдущий участник</BlockTitle>
+        <BlockHeader>Icon in the beginning of the list item</BlockHeader>
         <List>
-            {/* Additional "radio" prop to enable radio list item */}
-            <ListItem radio value="check_1" name="demo-radio" title="Чётное"></ListItem>
-            <ListItem radio value="check_2" name="demo-radio" title="Нечётное"></ListItem>
+            <ListItem radio value="check_1" name="demo-radio" title="Чётное" defaultChecked />
+            <ListItem radio value="check_2" name="demo-radio" title="Нечётное" />
         </List>
         <BlockTitle>Загадайте своё число для следующего игрока</BlockTitle>
         <Block className="text-align-center">
-          <Stepper fill></Stepper>
+          <Stepper fill onStepperChange={()=>{}}></Stepper>
         </Block>
         <Block>
            <Button fill>Отправить</Button>
