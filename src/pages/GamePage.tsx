6cb9d811ecb85f15248998f5a2b6ce8bae4204ee
Oleg Vilkov <oleg.vilkov.work@gmail.com>
2020-08-16 18:01:43 +0300

    Поддержана блокировка экрана  "Сейчас не ваш ход"

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import { f7, f7ready } from 'framework7-react';
 import { Page, Navbar } from 'framework7-react';
 import ScoreList from 'components/ScoreList';
 import GameMove from 'components/GameMove';
 
-export default function GamePage () {
+interface Props {
+    winner: string,
+    waitTurn: boolean,
+}
+
+export default function GamePage ({waitTurn}: Props) {
+
+    f7ready(() => {
+        // Пока участник ждёт других игроков, экран блокируется
+        if (waitTurn) {
+            f7.dialog.progress('Сейчас не ваш ход');
+        } else {
+            f7.dialog.close();
+        }
+
+    });
+
     return (
        <Page>
         <Navbar title="Game 1" />
