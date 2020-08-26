c79d66da6d50c52f4af7da9f6f5b4f8ab0c26dc4
Oleg Vilkov <oleg.vilkov.work@gmail.com>
2020-08-26 10:05:04 +0300

    Subscribed to current game in GamePage

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { subscribeToGame, unSubscribeFromGame } from 'redux/sagas/currentgame/actions';
+import { selectCurrentGame } from 'redux/reducers/currentgame/selector';
+import { IGameState } from 'redux/reducers/currentgame/types';
 
 import { f7, f7ready } from 'framework7-react';
 import { Page, Navbar } from 'framework7-react';
 import ScoreList from 'components/ScoreList';
 import Answer from 'components/Answer';
 
-interface Props {
-    winner: string,
-    waitTurn: boolean,
-}
+const mapState = (state: IGameState) => ({
+    game: selectCurrentGame(state),
+});
+
+const connector = connect(mapState, { subscribeToGame, unSubscribeFromGame });
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type PropsFromNavigation = {gameId: string};
+type Props = {winner: string, waitTurn: boolean} & PropsFromRedux & PropsFromNavigation;
 
 /**
  * Экран "Игра"
@@ -16,10 +25,18 @@ interface Props {
  * Пока участник ждёт других игроков, экран блокируется
  * Когда игра закончилась, выводим сообщение, что игра закончена и имя победителя
  */
-export default function GamePage ({winner, waitTurn}: Props) {
+function GamePage ({game, gameId='', winner, waitTurn, subscribeToGame, unSubscribeFromGame}: Props) {
 
     const redirectPath = '/';
+    const subtitle = game ? `${game.name} (${game.playersCount}/${game.playersForStart})` : '';
 
+    // Эффект прослушивания изменений в текущей игре
+    useEffect(() => {
+        subscribeToGame( gameId );
+        return ()=>{ unSubscribeFromGame() }
+    }, []);
+
+    // Эффект окончания игры
     useEffect(() => {
         if (f7.views.main.router.currentRoute.path == redirectPath) {
             return;
@@ -47,6 +64,7 @@ export default function GamePage ({winner, waitTurn}: Props) {
         }
     });
 
+    // Эфект блокировки игры
     useEffect(() => {
         if (!winner) {
             f7ready(() => {
@@ -62,9 +80,11 @@ export default function GamePage ({winner, waitTurn}: Props) {
 
     return (
        <Page loginScreen>
-        <Navbar title="Game 1"/>
+        <Navbar title="Game 1" subtitle={subtitle}/>
         <Answer />
         <ScoreList />
        </Page>
     )
 }
+
+export default connector(GamePage)
