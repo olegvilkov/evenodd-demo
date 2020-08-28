7cfe8f1a88f91c77032eb8f93c140c3169c619d5
Oleg Vilkov <oleg.vilkov.work@gmail.com>
2020-08-28 23:18:41 +0300

    Optimized Join Game Page

--- src/pages/JoinGamePage.tsx
+++ src/pages/JoinGamePage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
-import { selectUserName } from 'redux/reducers/user/selector';
-import { IGameState } from 'redux/reducers/currentgame/types';
+import { selectUser } from 'redux/reducers/user/selector';
 import { IUserState } from 'redux/reducers/user/types';
 
 import { f7 } from 'framework7-react';
@@ -9,21 +8,24 @@ import { Page, List, ListInput, ListButton } from 'framework7-react';
 import GameNavbar from 'components/GameNavbar';
 import withGameSubscription from 'hoc/GameSubscription';
 
-const mapState = (state: IGameState & IUserState) => ({
-    currentUsername: selectUserName(state),
+const mapState = (state: IUserState) => ({
+    user: selectUser(state),
 });
 
 const connector = connect(mapState, {});
 type PropsFromRedux = ConnectedProps<typeof connector>;
-type PropsFromNavigation = {gameId: string};
 
 /**
  * Экран "Присоединиться к игре"
  * @todo Добавить логику если игра заполнилась до вступления игрока
  */
-function JoinGamePage ({ currentUsername='', gameId=''}: PropsFromRedux & PropsFromNavigation) {
+function JoinGamePage ({ user }: PropsFromRedux) {
 
-    const [username, setUsername] = useState(currentUsername);
+    const [username, setUsername] = useState(user.name);
+
+    if (username != user.name) {
+        setUsername(user.name);
+    }
 
     return (
         <>
