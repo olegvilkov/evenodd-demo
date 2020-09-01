d7d892fb21596703d23ef972ec975b1ae81f050b
Oleg Vilkov <oleg.vilkov.work@gmail.com>
2020-09-01 12:57:02 +0300

    TDD: Should not set game winner if game round not finished

--- functions/test/test.ts
+++ functions/test/test.ts
@@ -22,7 +22,50 @@ describe("Winner function must work correct.", async () => {
 
     // TODO: Tests for 2 players (20 rounds limit)
 
+    it("Should not set game winner if game round not finished", async function () {
+        this.timeout(5000);
 
+        // Setup: Initialize game
+        const gameRef = db.doc('games/game1');
+        gameRef.set({
+            order: ['player1', 'player2'],
+            K: 10,
+        });
+    
+        // Setup: Initialize leader in points player
+        const player1Ref = gameRef.collection('players').doc('player1_id');
+        player1Ref.set({
+            round: 11,
+            points: 5,
+        });
+
+        // Setup: Initialize 2nd player
+        const player2Ref = gameRef.collection('players').doc('player2_id');
+        player2Ref.set({
+            round: 11,
+            points: 5,
+        });
+
+        // Trigger checkWinner cloud function
+        // Game round not finished becose player1 and player2 have different rounds
+        player2Ref.update({
+            round: 12,
+        });
+
+        // Listen first update to the game, it should return game winner
+        const winner = await Promise.race([
+            new Promise(resolve =>
+                unsubscribe = gameRef.onSnapshot(snap => {
+                    const data = snap.data();
+                    const winner = data && data.winner;
+                    if (winner) resolve(winner);
+                })
+            ),
+            new Promise(resolve => setTimeout(resolve, 3000, null))
+        ]);
+
+        expect(winner).be.null;
+    });
 
     it("Should set game winner if rounds > K and all players finish round turn", async function () {
 
@@ -49,6 +92,7 @@ describe("Winner function must work correct.", async () => {
             points: 50,
         }); 
 
+        // Trigger checkWinner cloud function
         // Game round finished when all players have same round
         player2Ref.update({
             round: 100,
@@ -90,6 +134,7 @@ describe("Winner function must work correct.", async () => {
             points: 5,
         });
 
+        // Trigger checkWinner cloud function
         // Game round finished when all players have same round
         player2Ref.update({
             round: 5,
