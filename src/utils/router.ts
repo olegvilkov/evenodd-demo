ea7c457c6048a08625efd1e180900239c080c7e2
Oleg Vilkov <oleg.vilkov.work@gmail.com>
2020-09-05 18:11:25 +0300

    Fix routing in join game

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -3,6 +3,6 @@ import { f7, f7ready } from 'framework7-react';
 export const navigate = (redirectPath: string) =>
     new Promise ((resolve) => {
         f7ready(() => {
-            resolve ( f7.views.main.router.back(redirectPath) )
+            resolve ( f7.views.main.router.navigate(redirectPath) )
         })
     })
